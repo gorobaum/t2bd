653b320965f9f4f788a90030c8c6eaec85220e8f
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2013-11-03 13:12:59 -0200

    Estrutura do BD MySQL

---
+++ Scripts-SQL/MySQL/estrutura.sql
@@ -0,0 +1,127 @@
+-- MySQL dump 10.14  Distrib 5.5.33a-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: cotacao
+-- ------------------------------------------------------
+-- Server version	5.5.33a-MariaDB-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `boleto`
+--
+
+DROP TABLE IF EXISTS `boleto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `boleto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pago` binary(1) NOT NULL DEFAULT '0',
+  `valor` decimal(10,0) NOT NULL,
+  `id_cambio` int(11) NOT NULL,
+  `data` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `boleto`
+--
+
+LOCK TABLES `boleto` WRITE;
+/*!40000 ALTER TABLE `boleto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `boleto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cambio`
+--
+
+DROP TABLE IF EXISTS `cambio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cambio` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_taxa_origem` int(11) NOT NULL,
+  `id_taxa_destino` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cambio`
+--
+
+LOCK TABLES `cambio` WRITE;
+/*!40000 ALTER TABLE `cambio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cambio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `moeda`
+--
+
+DROP TABLE IF EXISTS `moeda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `moeda` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `moeda`
+--
+
+LOCK TABLES `moeda` WRITE;
+/*!40000 ALTER TABLE `moeda` DISABLE KEYS */;
+/*!40000 ALTER TABLE `moeda` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `taxa`
+--
+
+DROP TABLE IF EXISTS `taxa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `taxa` (
+  `id` int(11) NOT NULL,
+  `valor` decimal(10,0) NOT NULL,
+  `data` datetime DEFAULT NULL,
+  `id_moeda` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `taxa`
+--
+
+LOCK TABLES `taxa` WRITE;
+/*!40000 ALTER TABLE `taxa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `taxa` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-11-03 13:11:56
