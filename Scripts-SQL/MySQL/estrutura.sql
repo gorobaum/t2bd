1d158be2eec333977da1b1261ae9fdad954d965b
Thiago de Gouveia Nunes <gorobaum@gmail.com>
2013-11-05 19:34:41 -0200

    Trigger

--- Scripts-SQL/MySQL/estrutura.sql
+++ Scripts-SQL/MySQL/estrutura.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.14  Distrib 5.5.33a-MariaDB, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.34, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: cotacao
 -- ------------------------------------------------------
--- Server version	5.5.33a-MariaDB-log
+-- Server version	5.5.34-0ubuntu0.13.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -114,6 +114,31 @@ LOCK TABLES `taxa` WRITE;
 /*!40000 ALTER TABLE `taxa` DISABLE KEYS */;
 /*!40000 ALTER TABLE `taxa` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER check_time BEFORE INSERT ON taxa 
+    FOR EACH ROW
+      DELETE FROM boleto WHERE boleto.id_cambio IN
+          (SELECT id FROM cambio WHERE
+             cambio.id_taxa_origem IN
+               (SELECT id FROM taxa WHERE 
+                  taxa.id_moeda = NEW.id_moeda AND (DAYOFTHEYEAR(CURDATE()) - DAYOFTHEYEAR(taxa.data)) <= 2)
+                  OR cambio.id_taxa_destino IN 
+                    (SELECT id FROM taxa WHERE taxa.id_moeda = NEW.id_moeda AND
+                    (DAYOFTHEYEAR(CURDATE()) - DAYOFTHEYEAR(taxa.data)) <= 2))
+                  AND boleto.pago = 0 AND (DAYOFTHEYEAR(CURDATE()) - DAYOFTHEYEAR(boleto.data)) <= 2 */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -124,4 +149,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-11-03 13:11:56
+-- Dump completed on 2013-11-05 19:34:15
