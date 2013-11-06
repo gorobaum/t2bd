c8c2aad15b8b325586182b88c9bc600079f15c0b
Thiago de Gouveia Nunes <gorobaum@gmail.com>
2013-11-06 11:06:55 -0200

    arrumando o trigger

--- Scripts-SQL/Triggers/trigger-update-taxa.sql
+++ Scripts-SQL/Triggers/trigger-update-taxa.sql
@@ -8,4 +8,4 @@ CREATE TRIGGER check_time BEFORE INSERT ON taxa
                   OR cambio.id_taxa_destino IN 
                     (SELECT id FROM taxa WHERE taxa.id_moeda = NEW.id_moeda AND
                     (DAYOFTHEYEAR(CURDATE()) - DAYOFTHEYEAR(taxa.data)) <= 2))
-                  AND boleto.pago = 0 AND (DAYOFTHEYEAR(CURDATE()) - DAYOFTHEYEAR(boleto.data)) <= 2;
+                  AND boleto.pago = 0 AND (DAYOFTHEYEAR(CURDATE()) - DAYOFTHEYEAR(boleto.data)) > 2;
